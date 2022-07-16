379b76e5b2edd0c96cc47301dab89ec21416bcdc
jj-2206 <jinju.woo2022@gmail.com>
2022-07-17 05:27:34 +0900

    fix: Add Module

--- webpack.config.js
+++ webpack.config.js
@@ -13,6 +13,16 @@ module.exports = {
     // filename: 'main.js',
     clean: true,
   },
+
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
+
   // 번들링 후 결과물의 처리 방식 등 다양한 플러그인들을 설정
   plugins: [
     new HtmlPlugin({
